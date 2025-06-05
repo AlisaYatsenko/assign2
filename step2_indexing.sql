fb195fe43277113907f17db5179ed7630b7c5505
AlisaYatsenko <ayatsenko@kse.org.ua>
2025-06-05 13:39:06 +0300

    Update step2_indexing.sql

--- step2_indexing.sql
+++ step2_indexing.sql
@@ -1,7 +1,5 @@
-CREATE INDEX idx_categories ON categories(CategoryName);
 CREATE INDEX idx_products1 ON products(CategoryID, ModifyDate);
-CREATE INDEX idx_products2 ON products(Resistant, Price);
-SELECT 
+explain analyze SELECT 
     c.CategoryName,
     COUNT(p.ProductID) AS TotalProducts,
     AVG(if(p.Resistant = 'durable', p.Price, null)) AS AvgResistantPrice,
@@ -13,7 +11,7 @@ FROM products p
 LEFT JOIN categories c ON c.CategoryID = p.CategoryID
 WHERE 
     c.CategoryName LIKE '%e%'
-    AND p.ModifyDate >= '2017-01-01' and p.ModifyDate <= '2017-12-31'
+    AND p.ModifyDate >= '2024-01-01' and p.ModifyDate <= '2024-12-31'
 GROUP BY c.CategoryName
 HAVING TotalProducts > 5
 ORDER BY AvgResistantPrice DESC;
